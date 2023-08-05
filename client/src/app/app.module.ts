8c21c001333d476ed6f5291f6949e15b5e0668f3
Sihle Mthembu <innocentsihle29@gmail.com>
2023-08-05 11:21:43 +0200

    Restored the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { SectionSalesComponent } from './Components/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './Components/section-orders/section-orders.component';
+import { SectionHealthComponent } from './Components/section-health/section-health.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
     BrowserModule,
