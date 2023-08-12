1f91c41ddc182512673375f8afe37726340fb7a7
Sihle Mthembu <innocentsihle29@gmail.com>
2023-08-12 09:34:19 +0200

    Added API Models

---
+++ Advantage.API/API/Data/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
---
+++ Advantage.API/API/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public string State { get; set; }
+
+    }
+}
