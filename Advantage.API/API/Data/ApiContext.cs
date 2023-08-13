b07208eb9f2c5cdffe0110356b5da3a7b44a6f31
Sihle Mthembu <innocentsihle29@gmail.com>
2023-08-13 07:10:55 +0200

    Added connection to database

--- Advantage.API/API/Data/ApiContext.cs
+++ Advantage.API/API/Data/ApiContext.cs
@@ -5,7 +5,7 @@ namespace API.Data
 {
     public class ApiContext : DbContext
     {
-        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+        public ApiContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
--- Advantage.API/API/Program.cs
+++ Advantage.API/API/Program.cs
@@ -1,24 +1,25 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApiContext>(opt => 
+{ 
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")
+        .Replace("{USERNAME}", builder.Configuration["Postgres:Username"])
+        .Replace("{PASSWORD}", builder.Configuration["Postgres:Password"]));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+// Configure the Http Request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
-app.UseAuthorization();
 
 app.MapControllers();
 
